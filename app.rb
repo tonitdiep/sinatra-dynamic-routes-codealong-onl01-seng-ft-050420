c6cb7bec48a086c0eab1974282fa904d4d590d86
Toni  Diep <adiep123@gmail.com>
2020-06-22 17:46:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,8 +21,8 @@ class App < Sinatra::Base
 
   get '/multiply/:num1/:num2' do
       binding.pry
-    @user_name = params[:num1]
-    @user_name = params[:num2]
+    @user_num1 = params[:num1]
+    @user_num2 = params[:num2]
     "Hello #{@user_name}!"
     # @user_num  = params[:num]
     # puts "#{params[:num1]} * #{params[:num2]}"
